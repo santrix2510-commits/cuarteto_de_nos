478c3475af74f85b958c183efd5eb5faa620778b
santi <santrix2510@gmail.com>
2026-07-28 18:56:10 -0300

    aprendi funciones flecha

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,8 @@
-function suma() {
+/*function suma() {
     console.log(2+2)
 }
-suma()
-
+suma()*/
+multiplicacion(9012,705)
 function saludable() {
     console.log("come frutas y verduras")
 }
@@ -17,3 +17,23 @@ function multiplicacion(num1,num2) {//las palabras dentro de este parentesis se 
     console.log(num1*num2)
 }
 multiplicacion(9012,705)
+
+// Parametro por defecto 
+function suma(num1,num2 = 0) {
+    return num1 + num2 //return no esta obligado a mostrar el resultado "en consola", PERO! si devuelve el resultado
+}
+console.log(1 + suma(900, ))    
+
+// FUNCION FLECHA
+/*let operacionMatematica = (uno, dos) => {
+    console.log(uno + dos)
+}
+    //FUNCION FLECHA RESUMIDA
+operacionMatematica(5, 892)
+operacionMatematica = (parametro1, parametro2) => parametro1 + parametro2
+ 
+
+    console.log(operacionMatematica(837, 9001))
+         FUNCION FLECHA AUN MAS RESUMIDA
+let operacionMatematica = parametro => parametro + 190
+    console.log(operacionMatematica(190))*/
